65a0c30ba1b737feb1e3da60eadf0eed89ea12d6
shelby <taras-official@protonmail.com>
2022-10-27 22:16:09 +0300

    update code lesson #2

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from colorama import init
+from colorama import Fore
+
+init()
+
+print(Fore.CYAN)
+
 a = float(input("Введите первое число : "))
 b = float(input("Введите второе число : "))
 
+print(Fore.GREEN)
+
 operation = input("Что сделать ? ( + , - , * , / ):")
+result = 0
 
 if operation == "+":
-    print(a + b )
+    result = a + b
 elif operation == "-":
-    print(a - b )
+    result = a - b
 elif operation == "*":
-    print(a * b)
+    result = a * b
 elif operation == "/":
-    print(a / b )
+    result = a / b
+
+print(Fore.YELLOW)
+
+print(f"Результат : {result} ")
